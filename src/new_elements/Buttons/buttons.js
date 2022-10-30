4735e53df228abe954410b626922c0e955539e33
Shureck <Sashalev200149@gmail.com>
2022-10-30 23:04:21 +0300

    second-first-take

--- src/new_elements/Buttons/buttons.js
+++ src/new_elements/Buttons/buttons.js
@@ -2,11 +2,11 @@ import React, { Component, useState, useEffect, useCallback}  from 'react';
 import './buttons.css';
 
 const ENDPOINT = process.env.REACT_APP_API_URL || "localhost:8060";
-
+const tt = process.env.REACT_APP_API_URL !== undefined ? "https://" : "http://";
 function Buttons(props) {
 
     const fetchRequest = useCallback(() => {
-        fetch('http://'+ENDPOINT+'/api/button?text='+props.name);
+        fetch(tt+ENDPOINT+'/api/button?text='+props.name);
     });
 
   return (
